ecb696219152574d78e05d7e0e8e5e72c2f99471
hollaze <deperiers.a@gmail.com>
2022-01-03 14:58:15 +0100

    need check

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 2
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 24
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 7
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 15
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 18
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 36
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 5
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 121
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 22
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
+
+Example:
+n = 9
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+Number of operations: 6
+"""
+
+
+def isPrimeNumber(n):
+    """
+    isPrimeNumber : Find is n is a prime number
+
+    Parameter :
+        (int) n : number
+
+    Return :
+        True if n is a prime number, False otherwise
+    """
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return (True)
+    return (False)
+
+
+def minOperations(n):
+    """
+    minOperations : Calculates the fewest number of
+                    operations needed to result in exactly n H
+    
+    Parameter :
+        (int) n : number of H to get
+    
+    Return :
+        Number of operations
+    """
+
+    numberOfActions = 0
+    divResult = 0
+    dividedBy = 0
+
+    if n <= 1:
+        return (numberOfActions)
+
+    if isPrimeNumber(n) == False:
+        numberOfActions = n
+        return (numberOfActions)
+
+    for i in range(2, n - 1):
+        if n % i == 0:
+            divResult = n / i
+            dividedBy = i
+            break
+
+    if n % 4 == 0 and n % 3 == 0:
+        numberOfActions = n / 4 + n / 3
+        return (int(numberOfActions))
+
+    # handles divided by 3 and all non prime numbers
+    elif isPrimeNumber(dividedBy) == False:
+        numberOfActions = dividedBy + divResult
+        return (int(numberOfActions))
+
+    return (numberOfActions)
