a6cb24d1fef21105ffa192215698ca97f2b2adbe
hollaze <deperiers.a@gmail.com>
2022-01-05 12:48:22 +0100

    coming back

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,10 +3,8 @@
 In a text file, there is a single character H.
 Your text editor can execute only two operations in this file:
 Copy All and Paste.
-
 Given a number n, write a method that calculates the fewest number of
 operations needed to result in exactly n H characters in the file.
-
 Example:
 n = 9
 H => Copy All => Paste => HH => Paste =>HHH =>
@@ -15,14 +13,26 @@ Number of operations: 6
 """
 
 
+def isPrimeNumber(n):
+    """
+    isPrimeNumber : Find is n is a prime number
+    Parameter :
+        (int) n : number
+    Return :
+        True if n is a prime number, False otherwise
+    """
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return (True)
+    return (False)
+
+
 def minOperations(n):
     """
     minOperations : Calculates the fewest number of
                     operations needed to result in exactly n H
-
     Parameter :
         (int) n : number of H to get
-
     Return :
         Number of operations
     """
@@ -33,7 +43,11 @@ def minOperations(n):
     dividedBy = 0
 
     if n <= 1:
-        return (minActionNumber)
+        return (numberOfActions)
+
+    if isPrimeNumber(n) is False:
+        numberOfActions = n
+        return (numberOfActions)
 
     for i in range(2, n - 1):
         if n % i == 0:
@@ -42,9 +56,7 @@ def minOperations(n):
             numberOfActions = divResult + dividedBy
             if minActionNumber == 0:
                 minActionNumber = numberOfActions
-            if numberOfActions < minActionNumber:
+            elif numberOfActions < minActionNumber:
                 minActionNumber = dividedBy + divResult
-            elif numberOfActions > minActionNumber:
-                return (int(minActionNumber))
 
-    return (n)
+    return (int(minActionNumber))
