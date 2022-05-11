df752991b83048c397f0ba45335e14a0ba851498
hollaze <deperiers.a@gmail.com>
2022-05-11 11:43:26 +0200

    header file

---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+int heap_extract(heap_t **root);
+heap_t *sort(heap_t *tmp);
+void preorderTraversal(heap_t *root, heap_t **node, size_t h, size_t l);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
