cd4f163caea736834f77ee84e110e15ec1dfaf49
hollaze <deperiers.a@gmail.com>
2022-05-18 10:50:54 +0200

    header file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts(char *str);
+int is_digit(char *s);
+void error_message(char *s);
+char *operations(char *num1, char *num2, int len1, int len2);
+
+#endif /* HOLBERTON_H */
