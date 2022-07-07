138b31d580cb78533ed11cd005e9ec84de4ca51b
hollaze <deperiers.a@gmail.com>
2022-07-07 16:15:57 +0200

    [Add] prototypes

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -7,5 +7,7 @@
 
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
+int find_max(int *array, size_t len);
+void radix_count_sort(int *array, int size, int dig);
 
 #endif /* SORT_H */
