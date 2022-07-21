246124aeb92842a52e8c228ad48dba6dcc882c1b
hollaze <deperiers.a@gmail.com>
2022-07-21 20:27:53 +0200

    avl binary tree

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * is_valid_avl - Check if a given tree is a valid avl tree
+ *
+ * @tree: head node of a tree
+ * @min: minimun value of a subtree
+ * @max: maximun value of a subtree
+ * @height: height of the tree
+ *
+ * Return: 1 on valid avl, 0 otherwise
+ */
+int is_valid_avl(const binary_tree_t *tree, int *height, int min, int max)
+{
+	int height_left = 0, height_right = 0;
+
+	if (tree == NULL)
+		return (1);
+
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+
+	if (is_valid_avl(tree->left, min, tree->n, &height_left) == 0 ||
+	    is_valid_avl(tree->right, tree->n, max, &height_right) == 0)
+		return (0);
+
+	*height = ((height_left > height_right) ? height_left : height_right) + 1;
+
+	if (abs(height_left - height_right) > 1)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * binary_tree_is_avl - check if a given tree is a valid AVL tree
+ * @tree: head node of a tree
+ * Return: 1 if a given tree is a valid AVL, 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	return (is_valid_avl(tree, &height, INT_MIN, INT_MAX));
+}
